7c50129628352bc5d9984f84b4cdd7eee2aa27b9
unknown <oanhtt@hblab/vn>
2026-01-14 10:58:25 +0700

    add file

--- doc/make.bat
+++ doc/make.bat
@@ -7,8 +7,8 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=.
-set BUILDDIR=_build
+set SOURCEDIR=vi
+set BUILDDIR=_build/vi
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
